9d2148e6fb1d8ad77407ad083f6e0faebcf5990e
nick fan <nickfan81@gmail.com>
2020-04-21 12:19:10 +0800

    fix: tweak setup script.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ python setup.py sdist
 python setup.py bdist_wheel
 python setup.py bdist_egg
 twine check dist/*
-python setup.py sdist upload
+#python setup.py sdist upload
+twine upload dist/*whl dist/*gz dist/*egg
